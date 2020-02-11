d13d44b571c0cb09f0106490448a2be2a9669c14
Nick Juelich <juelich23@gmail.com>
2020-02-11 13:21:00 -0600

    Updating master branch

--- ClientList.java
+++ ClientList.java
@@ -1,26 +1,66 @@
+/*
+Group 1
+CSCI 430
+Nick Juelich
+Majed Alsharikh
+Tyler Antony
+Abhishek Adhikari
+*/
+
 import java.util.*;
-import java.lang.*;
 import java.io.*;
-public class ClientList implements Serializable {
-	private static final long serialVersionUID = 1L;
-	private List clients = new LinkedList();
-	private static ClientList ClientList;
-	private ClientList(){}
-	public static ClientList instance() {
-		if (ClientList == null) {
-			return (ClientList = new ClientList());
-		}
-		else {
-			return ClientList;
-		}
-	}
-	
-	public boolean insertProduct(Product product){
-		clients.add(product);
-		return true;
-	}
-	
-	public Iterator getclients(){
-		return clients.iterator();
-	}
+
+public class ClientList implements Serializable{
+
+    private static final long serialVersionUID = 1L;
+    private List<Client> clients = new LinkedList();
+    private static ClientList clientList;
+    private ClientList() {
+    }
+    public static ClientList instance() {
+        if (clientList == null) {
+            return (clientList = new ClientList());
+        } else {
+            return clientList;
+        }
+    }
+
+    public boolean insertClient(Client client) {
+        clients.add(client);
+        return true;
+    }
+
+    public Iterator getClients(){
+        return clients.iterator();
+    }
+
+    private void writeObject(java.io.ObjectOutputStream output) {
+        try {
+            output.defaultWriteObject();
+            output.writeObject(clientList);
+        } catch(IOException ioe) {
+            ioe.printStackTrace();
+        }
+    }
+    private void readObject(java.io.ObjectInputStream input) {
+        try {
+            if (clientList != null) {
+                return;
+            } else {
+                input.defaultReadObject();
+                if (clientList == null) {
+                    clientList = (ClientList) input.readObject();
+                } else {
+                    input.readObject();
+                }
+            }
+        } catch(IOException ioe) {
+            ioe.printStackTrace();
+        } catch(ClassNotFoundException cnfe) {
+            cnfe.printStackTrace();
+        }
+    }
+    public String toString() {
+        return clients.toString();
+    }
 }
